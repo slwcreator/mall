6fb21dc38813209622ff2fa3a938907c5ba8f5bc
slwer <1047579002@qq.com>
2024-03-14 15:20:20 +0800

    feat: 增加静态资源映射配置

--- src/main/java/com/imooc/mall/config/ImoocMallWebMvcConfig.java
+++ src/main/java/com/imooc/mall/config/ImoocMallWebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.imooc.mall.config;
 
+import com.imooc.mall.common.Constant;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,5 +14,7 @@ public class ImoocMallWebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        //商品图片等定义静态资源映射
+        registry.addResourceHandler("/images/**").addResourceLocations("file:" + Constant.FILE_UPLOAD_DIR);
     }
 }
